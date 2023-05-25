9f352b37766f0c1dc4afd798ff15b32ee379d395
priyaank <priyaankk@gmail.com>
2023-05-25 18:21:46 +0530

    Added CSS settings

---
+++ build.js
@@ -0,0 +1,35 @@
+const { readFileSync, writeFile } = require('fs');
+const yaml = require('js-yaml');
+
+// Panda theme color definition
+const themeColors = yaml.load(readFileSync('themes/colors.yaml', 'utf-8'));
+// Base has the syntax tokens applicable across multiple languages
+let base = yaml.load(readFileSync('themes/catalyst-base.yaml', 'utf-8'));
+// Additional theme definitions to combine with base syntax token styles
+const workbench = yaml.load(readFileSync('themes/workbench.yaml', 'utf-8'));
+//Special for Specific languages
+const html = yaml.load(readFileSync('themes/html.yaml', 'utf-8'));
+const css = yaml.load(readFileSync('themes/css.yaml', 'utf-8'));
+
+// Merge workbench styles
+Object.assign(base, workbench);
+
+// Merge additional syntax token styles
+base.tokenColors = base.tokenColors.concat(html, css);
+
+// Stringify all of the combined theme styles so we can run string regexes on it to
+// replace color variables with color values
+base = JSON.stringify(base, null, 2);
+
+for (let color in themeColors) {
+	base = base.replace(new RegExp(color + '"', 'g'), themeColors[color] + '"');
+}
+
+// Base file has been extended with additional theme styles and color variables have
+// been replaced with Panda theme values. Write to /dist for consumption.
+writeFile('dist/Catalyst.json', base, (err) => {
+	if (err) {
+		console.warn(err);
+	}
+	console.log('Build finished');
+});
